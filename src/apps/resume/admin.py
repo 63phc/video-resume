13b0e78cdb5145ad3142c3ba8540e6aff0022039
postman17 <frompostal@gmail.com>
2019-02-11 20:44:02 +0500

    add models resume

--- src/apps/resume/admin.py
+++ src/apps/resume/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import Education, Skill, Job, Resume
 
-# Register your models here.
+
+@admin.register(Resume)
+class ResumeAdmin(admin.ModelAdmin):
+    list_display = [
+        'pk',
+        'other_skills',
+        'hobbies',
+        'about',
+        'get_educations',
+        'get_skills',
+        'get_jobs'
+    ]
+
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = [
+        'period_work',
+        'position',
+        'name_company'
+    ]
+
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = [
+        'name'
+    ]
+
+
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+    list_display = [
+        'period_edu',
+        'name_institution',
+        'faculty',
+        'form_study'
+    ]
