f6503ad5961e71eb2ec91d6f25093bb7da522480
Ravis <aao.t@yandex.ru>
2019-02-22 12:40:42 +0300

    review fixes

--- src/assets/webpack.js
+++ src/assets/webpack.js
@@ -42,12 +42,25 @@ module.exports = {
         new BundleTracker({ filename: '../static/webpack.stats.json' }),
 
         new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify('development'),
-          BASE_URL: JSON.stringify('http://0.0.0.0:8000/'),
+            'process.env': {
+              NODE_ENV: JSON.stringify('development'),
+              BASE_URL: JSON.stringify('http://0.0.0.0:8000/'),
+            }
+        }),
+        new webpack.ProvidePlugin({
+          "$":"jquery",
+          "jQuery":"jquery",
+          "window.jQuery":"jquery"
+        }),
+    ],
+    resolve : {
+        alias: {
+            // bind version of jquery-ui
+            "jquery-ui": "jquery-ui/jquery-ui.js",
+            // bind to modules;
+            modules: path.join(__dirname, "node_modules"),
         }
-      })
-    ]
+    }
 };
 
 // config.plugins = [
