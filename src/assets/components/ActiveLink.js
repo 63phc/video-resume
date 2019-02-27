82610b6af11102bf06fcef99dad85285a2611f31
Ravis <aao.t@yandex.ru>
2019-02-27 16:00:53 +0300

    review fixes

--- src/assets/components/ActiveLink.js
+++ src/assets/components/ActiveLink.js
@@ -1,16 +1,13 @@
-import $ from 'jquery';
-import Account from "./Account";
-
-let FOUNDED = 1;
+const FOUNDED = 1;
 let menu_links = ['role_choice', 'accounts', 'features', 'contacts'];
 
 function set_active_class_to (class_name) {
-    $('nav-link').removeClass('active');
-    $('.' + class_name).addClass('active');
+    document.querySelector('nav-link').removeClass('active');
+    document.querySelector('.' + class_name).addClass('active');
 }
 
 const ActiveLink = (() => {
-    $(document).ready(function() {
+    document.addEventListener('DOMContentLoaded', function () {
         for (const item of menu_links) {
             if (location.pathname.indexOf(item) == FOUNDED) {
                 set_active_class_to(item)
