b43184cda16bc7f152e6baba5fda6a0d784cb615
Ravis <aao.t@yandex.ru>
2019-02-28 10:50:55 +0300

    javascript fix

--- src/assets/components/ActiveLink.js
+++ src/assets/components/ActiveLink.js
@@ -1,19 +1,17 @@
 const FOUNDED = 1;
-let menu_links = ['role_choice', 'accounts', 'features', 'contacts'];
+const menuLinks = ['role_choice', 'accounts', 'features', 'contacts'];
 
-function set_active_class_to (class_name) {
-    document.querySelector('nav-link').removeClass('active');
-    document.querySelector('.' + class_name).addClass('active');
+function setActiveClass(class_name) {
+    document.querySelector('.nav-link').classList.remove("active");
+    document.querySelector('.' + class_name).classList.add('active');
 }
 
 const ActiveLink = (() => {
-    document.addEventListener('DOMContentLoaded', function () {
-        for (const item of menu_links) {
-            if (location.pathname.indexOf(item) == FOUNDED) {
-                set_active_class_to(item)
-            }
+    for (const item of menuLinks) {
+        if (location.pathname.indexOf(item) == FOUNDED) {
+            setActiveClass(item)
         }
-    })
+    }
 })();
 
 export default ActiveLink;
