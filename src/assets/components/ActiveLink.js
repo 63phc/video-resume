24db47a3ad8296b9596a82f3dac2a955dff0d668
Ravis <aao.t@yandex.ru>
2019-03-01 09:20:31 +0300

    arrow function

--- src/assets/components/ActiveLink.js
+++ src/assets/components/ActiveLink.js
@@ -1,16 +1,15 @@
 const FOUNDED = 1;
 const menuLinks = ['role_choice', 'accounts', 'features', 'contacts'];
 
-function setActiveClass(class_name) {
+function toggleActiveClass(class_name) {
     document.querySelector('.nav-link').classList.remove("active");
     document.querySelector('.' + class_name).classList.add('active');
 }
 
 const ActiveLink = (() => {
-    const current_menu = menuLinks.filter(function(link) {
-        return location.pathname.indexOf(link) == FOUNDED
-    })[0];
-    setActiveClass(current_menu)
+    const current_menu = menuLinks.find(
+        link => location.pathname.indexOf(link) == FOUNDED);
+    toggleActiveClass(current_menu);
 })();
 
 export default ActiveLink;
