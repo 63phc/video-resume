307cb504488b6eb7ccd51212087e7322d5d1c761
Ravis <aao.t@yandex.ru>
2019-03-04 13:49:02 +0300

    DashboardVeiw stage 1

--- src/apps/account_hr/urls.py
+++ src/apps/account_hr/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from .views import HRDashboardView
 
 urlpatterns = [
     path('', HRDashboardView.as_view(), name='main'),
--- src/apps/account_hr/views.py
+++ src/apps/account_hr/views.py
@@ -10,6 +10,10 @@ class HRDashboardView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(HRDashboardView, self).get_context_data(**kwargs)
-        context['vacancies'] = [{'title': 'Vacancy Number 1', }, ]
+        context['vacancies'] = [
+            {'title': 'Vacancy Number 1', 'description': 'descr for vac #1'},
+            {'title': 'Vacancy Number 2', 'description': 'descr for vac #2'},
+            {'title': 'Vacancy Number 3', 'description': 'descr for vac #3'},
+        ]
         # Vacancy.objects.filter(account_hr)
         return context
