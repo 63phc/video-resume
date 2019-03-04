a482f62f1229fadeb038a869849c49e19643ab54
Rdy90 <uart951@yandex.ru>
2019-03-04 15:19:03 +0700

    fix urls

--- src/core/urls.py
+++ src/core/urls.py
@@ -8,18 +8,25 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html')),
-    path('dashboard/worker/',
-         include('src.apps.account_worker.urls'),
-         name='dashboard_worker'),
-    path('role_choice/', TemplateView.as_view(
-         template_name='registration/role_choice.html'), name='sign_in'),
-    path('dashboard/hr/', TemplateView.as_view(
-         template_name='dashboard_hr/dashboard_hr.html'),
-         name='dashboard_hr'),
+    path(
+        'dashboard/worker/',
+        include('src.apps.account_worker.urls'),
+        name='dashboard_worker'
+    ),
+    path(
+        'role_choice/',
+        TemplateView.as_view(template_name='registration/role_choice.html'),
+        name='sign_in'
+    ),
+    path(
+        'dashboard/hr/',
+        TemplateView.as_view(template_name='dashboard_hr/dashboard_hr.html'),
+        name='dashboard_hr'
+    ),
     path('accounts/', include('src.apps.users.urls')),
     path('pages/', include('django.contrib.flatpages.urls')),
     path('vacancies/', include('src.apps.vacancy.urls'), name='vacancies'),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
 
 if settings.DEBUG:
