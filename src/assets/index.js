12615d708564304a294169e3bb8f9bb9151bf71e
Ravis <aao.t@yandex.ru>
2019-04-03 16:02:20 +0300

    review fix

--- src/assets/index.js
+++ src/assets/index.js
@@ -15,10 +15,10 @@ window.addEventListener('load', () => {
         const accountId = document.querySelector('#account_type');
         Account.setAttribute(accountId, document.location.pathname);
     }
-    // ActiveLink
-    if (document.location.pathname == '/') {
+    // Sliders
+    if (document.querySelector('.resume-slider')) {
         var slider = tns({
-            container: '.my-slider',
+            container: '.resume-slider',
             items: 2,
             slideBy: 'page',
             autoplay: false,
@@ -27,6 +27,8 @@ window.addEventListener('load', () => {
             controlsPosition: 'bottom',
             nav: false,
         });
+    }
+    if (document.querySelector('.vacancy-slider')) {
         var vacancy_slider = tns({
             container: '.vacancy-slider',
             items: 2,
@@ -38,10 +40,13 @@ window.addEventListener('load', () => {
             nav: false,
         });
     }
+
+    // ActiveLink
     const class_name = ActiveLink.getCurrentMenu(document.location.pathname);
     if (class_name) {
         ActiveLink.toggleActiveClass(class_name);
     }
+
     // WorkerSwitchLink
     if (document.querySelector('.worker-search-title')) {
         const titles = document.querySelectorAll('.worker-search-title');
